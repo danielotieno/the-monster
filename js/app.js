d9d44d29f978ec5a712a71571abdd590e04ace0c
danielotieno <danielotieno.ke@gmail.com>
2020-07-28 00:54:06 +0300

    ft:implement attack and check who win feature

--- js/app.js
+++ js/app.js
@@ -11,5 +11,40 @@ new Vue({
       this.monsterHealth = 100;
       this.playerHealth = 100;
     },
+
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You Won, New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('Monster Won, New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+
+    calculateDamage: function (max, min) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(10, 3);
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(10, 3);
+
+      this.checkWin();
+    },
   },
 });
